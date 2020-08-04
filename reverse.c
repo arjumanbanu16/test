8a495766ea2288111a8565f2aa92dcd4dcfee07b
arjumanbanu <arjumanbanu.a@gmail.com>
2020-08-04 16:17:12 +0530

    second

---
+++ reverse.c
@@ -0,0 +1,33 @@
+#include <stdio.h> 
+ 
+int main()
+ 
+{
+ 
+   int n, rev = 0;
+ 
+   printf("Please enter a number to reverse\n");
+ 
+   scanf(""%d"",&n);
+ 
+ 
+ 
+   while (n != 0)
+ 
+   {
+ 
+      rev = rev * 10;
+ 
+      rev = rev + n%10;
+ 
+      n = n/10;
+ 
+   }
+ 
+ 
+ 
+   printf("The reverse of entered number is %d\n", rev);
+ 
+   return 0;
+ 
+}
